43a2e496564c20d9ccfe395a430e6c18905eebf7
JSeHoone <beebea545@gmail.com>
2025-12-07 21:05:34 +0900

    refactor : LoginRequest `@NotBlank` 어노테이션 추가
    - email, password가 빈값이 들어오지 못하도록 어노테이션을 추가하였습니다.

--- src/main/java/com/teamexp/learnflowapi/auth/controller/dto/LoginRequest.java
+++ src/main/java/com/teamexp/learnflowapi/auth/controller/dto/LoginRequest.java
@@ -1,14 +1,17 @@
 package com.teamexp.learnflowapi.auth.controller.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
         @Email
         @NotNull(message = "Email cannot be null")
+        @NotBlank(message = "Email cannot be blank")
         String email,
 
         @NotNull(message = "Password cannot be null")
+        @NotBlank(message = "Password cannot be blank")
         String password
 ) {
 }
