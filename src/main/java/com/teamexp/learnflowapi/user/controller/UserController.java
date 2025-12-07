a6094333b4b85b4e80d170152962500d0a8d94fd
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-12-07 21:01:32 +0900

    [Stacked-feat/user-login] FEAT[user]: 닉네임 중복 여부 확인 API (#11)
    * refactor : `/api/v1/users/check` API 필터 검증 제외
    
    - 사용 가능한 이름인지 확인하는 API
    
    * feat : 닉네임 중복 확인 API 추가
    
    * refactor : users/check API 쿼리파라미터 검증 추가
    
    - `@NotBlank` 어노테이션을 추가하여 빈 값이 들어오는 경우 오류가 발생하도록 처리하였습니다.

--- src/main/java/com/teamexp/learnflowapi/user/controller/UserController.java
+++ src/main/java/com/teamexp/learnflowapi/user/controller/UserController.java
@@ -4,12 +4,15 @@ import com.teamexp.learnflowapi.global.response.BaseResponse;
 import com.teamexp.learnflowapi.user.controller.dto.UserCreateRequest;
 import com.teamexp.learnflowapi.user.service.UserService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,5 +33,11 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(BaseResponse.ok(null));
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<BaseResponse<?>> checkNickname(@RequestParam(name = "nickname") @NotBlank String nickname) {
+        boolean existsNickname = userService.checkNickname(nickname);
+        return ResponseEntity.ok(BaseResponse.ok(existsNickname));
     }
 }
