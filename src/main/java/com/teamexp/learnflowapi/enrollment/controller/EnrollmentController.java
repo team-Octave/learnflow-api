dfa062d506cb728672bb9b6f205edad61b6be4d0
JinYong833 <jjystudy1@gmail.com>
2025-12-07 17:10:37 +0900

    feat[Enrollment] : 내 수강목록 조회 및 강좌 선택 컨트롤러 구현

--- src/main/java/com/teamexp/learnflowapi/enrollment/controller/EnrollmentController.java
+++ src/main/java/com/teamexp/learnflowapi/enrollment/controller/EnrollmentController.java
@@ -1,16 +1,17 @@
 package com.teamexp.learnflowapi.enrollment.controller;
 
+import com.teamexp.learnflowapi.enrollment.dto.DecidedEnrollmentRequest;
 import com.teamexp.learnflowapi.enrollment.dto.EnrollmentRequest;
 import com.teamexp.learnflowapi.enrollment.dto.EnrollmentResponse;
+import com.teamexp.learnflowapi.enrollment.dto.GetEnrollmentRequest;
 import com.teamexp.learnflowapi.enrollment.service.EnrollmentService;
 import com.teamexp.learnflowapi.global.response.BaseResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/enrollment")
@@ -26,5 +27,17 @@ public class EnrollmentController {
     public ResponseEntity<BaseResponse<EnrollmentResponse>> create(@Valid @RequestBody EnrollmentRequest request) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponse.ok(enrollmentService.createEnrollment(request)));
+    }
+
+    @GetMapping
+    public ResponseEntity<BaseResponse<List<EnrollmentResponse>>> getEnrollments(@Valid @RequestBody GetEnrollmentRequest request) {
+
+        return ResponseEntity.ok().body(BaseResponse.ok(enrollmentService.getEnrollments(request)));
+    }
+
+    @PostMapping("/click")
+    public ResponseEntity<Void> click(@Valid @RequestBody DecidedEnrollmentRequest request) {
+        enrollmentService.updateEnrollment(request);
+        return ResponseEntity.ok().build();
     }
 }
