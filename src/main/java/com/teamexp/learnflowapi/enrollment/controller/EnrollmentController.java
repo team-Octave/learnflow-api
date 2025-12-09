c3b259aa4824accf1a0e83e280fb4af5dfea794a
JinYong833 <jjystudy1@gmail.com>
2025-12-09 17:36:44 +0900

    feat[enrollment] : controller에 deleteEnrollment 추가
    - enrollment 삭제 추가
    - TODO 작성

--- src/main/java/com/teamexp/learnflowapi/enrollment/controller/EnrollmentController.java
+++ src/main/java/com/teamexp/learnflowapi/enrollment/controller/EnrollmentController.java
@@ -8,6 +8,7 @@ import com.teamexp.learnflowapi.enrollment.service.EnrollmentService;
 import com.teamexp.learnflowapi.global.response.BaseResponse;
 import com.teamexp.learnflowapi.global.security.principal.CustomUserPrincipal;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,12 +16,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// TODO Response 세분화 후 Refactor 적용 예정
 @RestController
 @RequestMapping("api/v1/enrollment")
 public class EnrollmentController {
 
     private final EnrollmentService enrollmentService;
 
+    @Autowired
     public EnrollmentController(EnrollmentService enrollmentService) {
         this.enrollmentService = enrollmentService;
     }
@@ -29,28 +32,38 @@ public class EnrollmentController {
     public ResponseEntity<BaseResponse<EnrollmentResponse>> createEnrollment(@AuthenticationPrincipal CustomUserPrincipal principal,
                                                                              @Valid @RequestBody EnrollmentRequest request) {
 
+        // 생성 성공 반환
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(BaseResponse
                         .ok(enrollmentService.createEnrollment(principal.getId(), request)));
-    }
-
-    @GetMapping
-    public ResponseEntity<BaseResponse<List<EnrollmentResponse>>> getEnrollment(@AuthenticationPrincipal CustomUserPrincipal principal) {
-
-        return ResponseEntity.ok(BaseResponse.ok(enrollmentService.getEnrollments(principal.getId())));
-    }
-
-    // TODO 테스트용
-    @PostMapping("/select-enrollment")
-    public ResponseEntity<Void> selectEnrollment(@Valid @RequestBody DecidedEnrollmentRequest request) {
-        enrollmentService.updateEnrollment(request);
-        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/complete-lesson")
     public ResponseEntity<Void> completeLesson(@AuthenticationPrincipal CustomUserPrincipal principal,
                                                @Valid @RequestBody CreateCompletedLessonRequest request) {
         enrollmentService.createCompletedLesson(principal.getId() , request);
+        // 완료한 lessonId, enrollmentId?
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/select-enrollment")
+    public ResponseEntity<Void> selectEnrollment(@Valid @RequestBody DecidedEnrollmentRequest request) {
+        enrollmentService.updateEnrollment(request);
+        // lecture정보 > 챕터 > 레슨(완료 true만), 가장 마지막에 완료된 레슨 ID, 진도율
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<BaseResponse<List<EnrollmentResponse>>> getEnrollment(@AuthenticationPrincipal CustomUserPrincipal principal) {
+
+        // lecture 썸네일, lecture 이름, 수강률, review 별점, review수강평, 생성일, 업데이트일, Status 반환
+        return ResponseEntity.ok(BaseResponse.ok(enrollmentService.getEnrollments(principal.getId())));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteEnrollment(@AuthenticationPrincipal CustomUserPrincipal principal,
+                                                 @Valid @RequestBody DecidedEnrollmentRequest request) {
+        enrollmentService.deleteEnrollment(principal.getId() , request);
         return ResponseEntity.ok().build();
     }
 }
