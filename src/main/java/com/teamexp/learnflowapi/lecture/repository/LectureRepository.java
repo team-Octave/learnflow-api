8ee39d7f2d5d993f8f89499ffc3d73a0ed8b06ff
yeyeyey3941 <cjttkfkd3941@gmail.com>
2025-12-09 16:35:50 +0900

    feat : Lecture repository define

--- src/main/java/com/teamexp/learnflowapi/lecture/repository/LectureRepository.java
+++ src/main/java/com/teamexp/learnflowapi/lecture/repository/LectureRepository.java
@@ -2,15 +2,11 @@ package com.teamexp.learnflowapi.lecture.repository;
 
 import com.teamexp.learnflowapi.lecture.model.Lecture;
 import com.teamexp.learnflowapi.lecture.model.LectureStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface LectureRepository  {
 
     Lecture save(Lecture lecture);
