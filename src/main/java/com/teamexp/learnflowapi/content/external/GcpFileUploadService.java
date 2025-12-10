c5a4fb36182ea00058039ccb9cf19180ca803580
Taehun3536 <hg1282rnjs@gmail.com>
2025-12-10 18:13:15 +0900

    feat/[content] GcpFileUploadService 코드 수정

--- src/main/java/com/teamexp/learnflowapi/content/external/GcpFileUploadService.java
+++ src/main/java/com/teamexp/learnflowapi/content/external/GcpFileUploadService.java
@@ -7,17 +7,14 @@ import com.google.cloud.storage.StorageOptions;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
-import com.google.cloud.storage.Blob;
-
 
 @Service
-@RequiredArgsConstructor
 public class GcpFileUploadService {
 
     @Value("${spring.cloud.gcp.storage.bucket-name}")
@@ -26,27 +23,47 @@ public class GcpFileUploadService {
     @Value("${spring.cloud.gcp.storage.json-key-location}")
     private String keyFileName;
 
-    public String createUploadUrl(String uploadFileName,  MultipartFile file) throws IOException {
-
+    private Storage createStorage() throws IOException {
         InputStream keyFile = ResourceUtils.getURL("classpath:" + keyFileName).openStream();
 
-        Storage storage = StorageOptions.newBuilder()
+        return StorageOptions.newBuilder()
                 .setCredentials(GoogleCredentials.fromStream(keyFile))
                 .build()
                 .getService();
+    }
 
-        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, uploadFileName)
-                .build();
+    /**
+     * 공통 업로드 로직
+     */
+    public void uploadFile(String fileName, MultipartFile file) throws IOException {
+        Storage storage = createStorage();
 
-        Blob blob = (Blob) storage.create(blobInfo, file.getInputStream());
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, fileName).build();
+
+        storage.create(blobInfo, file.getInputStream());
+    }
+
+    /**
+     * 공개 리소스용 Public URL 생성
+     */
+    public String createPublicUrl(String fileName) {
+        return "https://storage.googleapis.com/learnflow-file-storage" + bucketName + "/" + fileName;
+    }
+
+    /**
+     * 비공개 리소스용 Signed URL 생성
+     */
+    public String createSignedUrl(String fileName, long minutes) throws IOException {
+        Storage storage = createStorage();
+
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, fileName).build();
 
         URL signedUrl = storage.signUrl(
-                blob,
-                15,
-                java.util.concurrent.TimeUnit.MINUTES,
+                blobInfo,
+                minutes,
+                TimeUnit.MINUTES,
                 Storage.SignUrlOption.withV4Signature()
         );
-
 
         return signedUrl.toString();
     }
