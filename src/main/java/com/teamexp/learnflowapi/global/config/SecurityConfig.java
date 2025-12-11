8bf3a937f8b0bd27d9c14cfec51d1f83f582a476
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2025-12-11 19:43:14 +0900

    REFACTOR[GLOBAL] : 전체 강의조회, 강의 단 건 조회, 강의 리뷰 조회 필터에 제거 (#56)
    * refactor : 전체 강의조회, 강의 단 건 조회, 강의 리뷰 조회 필터에 제거
    
    * refactor : 내 정보 조회 authenticated 처리 되도록 설정 추가

--- src/main/java/com/teamexp/learnflowapi/global/config/SecurityConfig.java
+++ src/main/java/com/teamexp/learnflowapi/global/config/SecurityConfig.java
@@ -46,6 +46,10 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/users/check").permitAll()    // 닉네임 체크
                         .requestMatchers("/api/v1/auth/login").permitAll()    // 로그인
                         .requestMatchers("/api/v1/auth/reissue").permitAll()   // 토큰 재발급
+                        .requestMatchers(HttpMethod.GET, "/api/v1/lectures/my").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/lectures").permitAll() // 강의 전체 조회
+                        .requestMatchers(HttpMethod.GET, "/api/v1/lectures/*").permitAll() // 강의 단건 조회
+                        .requestMatchers(HttpMethod.GET, "/api/v1/reviews/lectures/*").permitAll() // 강의별 리뷰 조회
                         .anyRequest().authenticated()
                 )
                 .userDetailsService(userDetailsService);
