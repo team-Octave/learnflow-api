6a3bf9549d086862c7371a0ff2b6e4a803dcf2e3
Taehun3536 <hg1282rnjs@gmail.com>
2025-12-11 15:12:17 +0900

    FEAT\Content QuizController RequestMapping '/' 추가

--- src/main/java/com/teamexp/learnflowapi/content/controller/QuizController.java
+++ src/main/java/com/teamexp/learnflowapi/content/controller/QuizController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/contents")
+@RequestMapping("api/v1/contents/")
 public class QuizController {
 
     private final QuizService quizService;
