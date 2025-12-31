82b92d0a71916fa7763e31bff5dd8e8a98ffb431
woongblack <woongblack123@gmail.com>
2026-01-01 06:05:42 +0900

    refactor: 네이티브 쿼리를 제거하고 JPA 메서드로 변경

--- src/main/java/com/teamexp/learnflowapi/review/repository/ReviewRepository.java
+++ src/main/java/com/teamexp/learnflowapi/review/repository/ReviewRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     // 중복 작성 방지
     // 변경: Enrollment 객체 자체로 존재 여부 확인(1인1리뷰 정책 최적화)
@@ -17,4 +19,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     // 내 리뷰 조회
     // 변경: Review -> Enrollment -> UserId 순으로 접근
     Page<Review> findByEnrollment_UserId(String userId, Pageable pageable);
+
+    List<Review> findByEnrollment_IdIn(List<Long> enrollmentIds);
+
 }
