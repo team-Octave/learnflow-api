2384f095b78468da222572459f0f0295230601d5
JSeHoone <beebea545@gmail.com>
2026-01-15 02:02:40 +0900

    feat[approval] : ApprovalRejectReason Embeddable에 from 메서드 추가

--- src/main/java/com/teamexp/learnflowapi/admin/model/ApprovalRejectReason.java
+++ src/main/java/com/teamexp/learnflowapi/admin/model/ApprovalRejectReason.java
@@ -2,6 +2,7 @@ package com.teamexp.learnflowapi.admin.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import java.util.List;
 
 @Embeddable
 public class ApprovalRejectReason {
@@ -20,5 +21,27 @@ public class ApprovalRejectReason {
 
     @Column(name = "other")
     private Boolean other;
+
+    protected ApprovalRejectReason() {}
+
+    public static ApprovalRejectReason from(List<ApprovalRejectType> types) {
+        // 승인 허가를 내리는 경우에는 null을 반환
+        if (types == null || types.isEmpty()) {
+            return null;
+        }
+
+        ApprovalRejectReason reason = new ApprovalRejectReason();
+
+        for (ApprovalRejectType type : types) {
+            switch (type) {
+                case CONTENT_QUALITY_LOW -> reason.contentQualityLow = true;
+                case LECTURE_INFO_MISMATCH -> reason.lectureInfoMismatch = true;
+                case MEDIA_QUALITY_ISSUE -> reason.mediaQualityIssue = true;
+                case POLICY_VIOLATION -> reason.policyViolations = true;
+                case OTHER -> reason.other = true;
+            }
+        }
+        return reason;
+    }
 }
 
