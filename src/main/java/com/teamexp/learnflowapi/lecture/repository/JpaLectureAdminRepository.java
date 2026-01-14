c7ea4c999fdd7963171a5f3b203157680f255a2c
yeyeyey3941 <cjttkfkd3941@gmail.com>
2026-01-14 10:52:55 +0900

    refactor[Lecture] : Update admin lecture queries with pagination support

--- src/main/java/com/teamexp/learnflowapi/lecture/repository/JpaLectureAdminRepository.java
+++ src/main/java/com/teamexp/learnflowapi/lecture/repository/JpaLectureAdminRepository.java
@@ -3,6 +3,8 @@ package com.teamexp.learnflowapi.lecture.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,14 +14,16 @@ import com.teamexp.learnflowapi.lecture.model.Lecture;
 @Repository
 public interface JpaLectureAdminRepository extends JpaRepository<Lecture, Long>, LectureAdminRepository {
     
-    @Override // Admin에서 조회할 강의 id 목록을 받아서 강의 목록을 조회
+    @Override // Admin에서 조회할 강의 id 목록을 받아서 updatedAt이 오래된 강의 목록을 조회
     @Query("SELECT l FROM Lecture l " +
-    "WHERE l.id IN :lectureIds AND l.deleteFlag = false AND l.status = 'SUBMITTED'")
-    List<Lecture> findAllByIds(List<Long> lectureIds);
+    "WHERE l.id IN :lectureIds AND l.deleteFlag = false AND l.status = 'SUBMITTED' " +
+    "ORDER BY l.updatedAt DESC NULLS LAST")
+    Page<Lecture> findAllByIds(List<Long> lectureIds, Pageable pageable);
 
-    @Override // Status가 SUBMITTED인 강의 목록을 조회
-    @Query("SELECT l FROM Lecture l WHERE l.status = 'SUBMITTED' AND l.deleteFlag = false")
-    List<Lecture> findAllSubmittedLectures();
+    @Override // Status가 SUBMITTED인 updatedAt이 오래된 강의 목록을 조회
+    @Query("SELECT l FROM Lecture l WHERE l.status = 'SUBMITTED' AND l.deleteFlag = false " +
+    "ORDER BY l.updatedAt DESC NULLS LAST")
+    Page<Lecture> findAllSubmittedLectures(Pageable pageable);
     
     @Override // findById - to change status from submitted to available or rejected
     @Query("SELECT l FROM Lecture l " + 
@@ -34,4 +38,5 @@ public interface JpaLectureAdminRepository extends JpaRepository<Lecture, Long>,
         "WHERE l.id = :lectureId AND l.deleteFlag = false AND l.status = 'SUBMITTED'")
     Optional<Lecture> findByIdWithChaptersAndLessonsAndQuizzes(Long lectureId);
 }
+
 
--- src/main/java/com/teamexp/learnflowapi/lecture/repository/LectureAdminRepository.java
+++ src/main/java/com/teamexp/learnflowapi/lecture/repository/LectureAdminRepository.java
@@ -3,20 +3,24 @@ package com.teamexp.learnflowapi.lecture.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.teamexp.learnflowapi.lecture.model.Lecture;
 
 public interface LectureAdminRepository {
 
-    // Admin에서 조회할 강의 id 목록을 받아서 강의 목록을 조회
-    List<Lecture> findAllByIds(List<Long> lectureIds);
+    // Admin에서 조회할 강의 id 목록을 받아서 강의 목록을 updatedAt이 오래된 순서로 조회
+    Page<Lecture> findAllByIds(List<Long> lectureIds, Pageable pageable);
 
-    // Status가 SUBMITTED인 강의 목록을 조회
-    List<Lecture> findAllSubmittedLectures();
+    // Status가 SUBMITTED인 강의 목록을 updatedAt이 오래된 순서로 조회
+    Page<Lecture> findAllSubmittedLectures(Pageable pageable);
 
     // findById - to change status from submitted to available or rejected
     Optional<Lecture> findById(Long lectureId);
 
     // findByIdWithChaptersAndLessonsAndQuizzes - use check lecture detail
     Optional<Lecture> findByIdWithChaptersAndLessonsAndQuizzes(Long lectureId);
+
     
 }
