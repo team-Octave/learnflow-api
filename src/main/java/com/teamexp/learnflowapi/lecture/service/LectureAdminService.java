3294a52e03ee72027671879fd63093e3085472cb
yeyeyey <cjttkfkd3941@gmail.com>
2026-01-14 11:34:47 +0900

    feat[Lecture]: Admin 강의 관리 기능 추가 (#81)
    * feat[Lecture]: Admin 강의 관리 기능 추가
    1. LectureAdminRepository/Service 구현
    
    * refactor[Lecture] : Update admin lecture queries with pagination support

--- src/main/java/com/teamexp/learnflowapi/lecture/service/LectureAdminService.java
+++ src/main/java/com/teamexp/learnflowapi/lecture/service/LectureAdminService.java
@@ -1,32 +1,38 @@
 package com.teamexp.learnflowapi.lecture.service;
 
+import com.teamexp.learnflowapi.lecture.exception.LectureNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.teamexp.learnflowapi.lecture.model.Lecture;
+import com.teamexp.learnflowapi.lecture.repository.LectureAdminRepository;
 
 @Service
-@Transactional(readOnly = true)
 public class LectureAdminService {
 
-    private final LectureService lectureService;
+    private final LectureAdminRepository lectureAdminRepository;
 
-    public LectureAdminService(LectureService lectureService) {
-        this.lectureService = lectureService;
+    public LectureAdminService(LectureAdminRepository lectureAdminRepository) {
+        this.lectureAdminRepository = lectureAdminRepository;
     }
 
     // Only Admin can call this method
     // check admin at controller
+    @Transactional
     public void allowPublishLecture(Long lectureId) {
-        Lecture lecture = lectureService.findLectureWithChaptersAndLessons(lectureId);
+        Lecture lecture = lectureAdminRepository.findById(lectureId)
+            .orElseThrow(LectureNotFoundException::new);
 
         lecture.allowPublish();
+        
     }
 
     // Only Admin can call this method
     // check admin at controller
+    @Transactional
     public void notAllowPublishLecture(Long lectureId) {
-        Lecture lecture = lectureService.findLectureWithChaptersAndLessons(lectureId);
+        Lecture lecture = lectureAdminRepository.findById(lectureId)
+            .orElseThrow(LectureNotFoundException::new);
 
         lecture.notAllowPublish();
     }
