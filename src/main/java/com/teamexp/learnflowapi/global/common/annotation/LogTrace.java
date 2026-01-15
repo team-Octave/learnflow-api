d9ed6ef559177b360a01d89dfa12b216d0d3a730
Sung Ryul Cho <hahahoho5811@gmail.com>
2026-01-15 22:28:03 +0900

    fix[global] : 코드래빗 피드백 반영 - 어노테이션 JavaDoc 주석 추가

--- src/main/java/com/teamexp/learnflowapi/global/common/annotation/LogTrace.java
+++ src/main/java/com/teamexp/learnflowapi/global/common/annotation/LogTrace.java
@@ -2,6 +2,10 @@ package com.teamexp.learnflowapi.global.common.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 메서드 실행 궤적(Trace)을 로깅하기 위한 마커 어노테이션입니다.
+ * 이 어노테이션이 부착된 메서드는 실행 시간과 시작/종료 로그가 자동으로 기록됩니다.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LogTrace {
