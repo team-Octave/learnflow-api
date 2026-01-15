7a703edaf1e1c6ca7aef8230a6c7e1668428a688
Taehun3536 <hg1282rnjs@gmail.com>
2026-01-15 19:34:38 +0900

    feat/content- ContentMediaService에 getStreamingUrl 코드 추가

--- src/main/java/com/teamexp/learnflowapi/content/service/ContentMediaService.java
+++ src/main/java/com/teamexp/learnflowapi/content/service/ContentMediaService.java
@@ -6,13 +6,13 @@ import com.teamexp.learnflowapi.content.dto.UploadInitRequest;
 import com.teamexp.learnflowapi.content.dto.UploadInitResponse;
 import com.teamexp.learnflowapi.content.exception.FileNameEmptyException;
 import com.teamexp.learnflowapi.content.exception.InvalidVideoExtensionException;
+import com.teamexp.learnflowapi.content.exception.LessonVideoNotFoundException;
 import com.teamexp.learnflowapi.content.exception.MediaAlreadyCompletedException;
 import com.teamexp.learnflowapi.content.exception.MediaNotFoundException;
 import com.teamexp.learnflowapi.content.external.GcpSignedUrlService;
 import com.teamexp.learnflowapi.content.model.ContentMedia;
 import com.teamexp.learnflowapi.content.model.MediaStatus;
 import com.teamexp.learnflowapi.content.repository.ContentMediaRepository;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,8 +30,6 @@ public class ContentMediaService {
         this.gcpSignedUrlService = gcpSignedUrlService;
     }
 
-    @Value("${spring.cloud.gcp.storage.bucket-name}")
-    private String bucketName;
 
     /*
      * 1. 영상 파일 형식 .mp4
@@ -90,6 +88,22 @@ public class ContentMediaService {
 
         // 업로드 상태 업데이트
         media.completeUpload(uploadCompleteRequest.durationSec());
+    }
+
+    public String getStreamingUrl (Long lessonId){
+        // 레슨 id조회
+        ContentMedia media =contentMediaRepository.findById(lessonId)
+                .orElseThrow(LessonVideoNotFoundException::new);
+
+        try {
+
+            return gcpSignedUrlService.streamingCreateSignedUrl(
+                    media.getFileKey(),
+                    media.getDurationSec()
+            );
+        } catch (Exception e){
+            throw new IllegalArgumentException("영상 재생 URL 생성 중 오류가 발생했습니다.");
+        }
     }
 
     private void validateInitRequest(UploadInitRequest req) {
