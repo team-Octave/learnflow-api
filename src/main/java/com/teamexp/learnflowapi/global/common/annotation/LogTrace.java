8f54db46cda5d88291ac3b8108de4910be69c6a8
Sung Ryul Cho <hahahoho5811@gmail.com>
2026-01-15 21:52:52 +0900

    feat[global] : 로깅 추적용 커스텀 어노테이션 생성

---
+++ src/main/java/com/teamexp/learnflowapi/global/common/annotation/LogTrace.java
@@ -0,0 +1,8 @@
+package com.teamexp.learnflowapi.global.common.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogTrace {
+}
