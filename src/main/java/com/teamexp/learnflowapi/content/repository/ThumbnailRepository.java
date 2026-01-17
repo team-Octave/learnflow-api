c9fbe9d528c0fe7c9bda4441bc1bbe2f84750e17
yeyeyey <cjttkfkd3941@gmail.com>
2026-01-17 16:46:59 +0900

    hotfix[lecture]:thumbnail repo 변경 (#109)

--- src/main/java/com/teamexp/learnflowapi/content/repository/ThumbnailRepository.java
+++ src/main/java/com/teamexp/learnflowapi/content/repository/ThumbnailRepository.java
@@ -2,6 +2,7 @@ package com.teamexp.learnflowapi.content.repository;
 
 import com.teamexp.learnflowapi.content.model.Thumbnail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -15,6 +16,7 @@ public interface ThumbnailRepository extends JpaRepository<Thumbnail,Long> {
     Optional<Thumbnail> findByLectureId(Long lectureId);
 
     @Deprecated // TODO: 모든 lecture의 thumbnailUrl 역정규화 완료 후 제거, 사실 그냥 thumbnai 자체가 필요없음
+    @Query("SELECT t.fileUrl FROM Thumbnail t WHERE t.id = :thumbnailId")
     String findFileUrlById(Long thumbnailId);
 
     // 렉처 삭제시 같이 삭제
