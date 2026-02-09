035b82fe89c46815ddd3c5a1dce2cb354773ba90
dkstpwns10 <dkstpwns10@naver.com>
2026-02-10 03:39:58 +0900

    feat[membership]: 만료된 멤버십 삭제를 위한 스케줄러 추가

--- src/main/java/com/teamexp/learnflowapi/LearnflowApiApplication.java
+++ src/main/java/com/teamexp/learnflowapi/LearnflowApiApplication.java
@@ -3,9 +3,11 @@ package com.teamexp.learnflowapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class LearnflowApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/teamexp/learnflowapi/membership/repository/MembershipRepository.java
+++ src/main/java/com/teamexp/learnflowapi/membership/repository/MembershipRepository.java
@@ -3,6 +3,7 @@ package com.teamexp.learnflowapi.membership.repository;
 import com.teamexp.learnflowapi.membership.model.Membership;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
@@ -11,4 +12,6 @@ public interface MembershipRepository extends JpaRepository<Membership, Long> {
     boolean existsByUserId(String userId);
 
     Optional<Membership> findFirstByUserIdOrderByExpiredAtDesc(String userId);
+
+    void deleteAllByExpiredAtBefore(LocalDateTime expiredAt);
 }
