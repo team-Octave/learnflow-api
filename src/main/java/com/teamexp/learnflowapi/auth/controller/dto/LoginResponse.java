f281b2ca382a88a83b28a9caff15ea51008d6c32
dkstpwns10 <dkstpwns10@naver.com>
2026-02-10 13:51:54 +0900

    feat[user]: Auth & User 유저정보 조회 메서드에 멤버십 정보 포함

--- src/main/java/com/teamexp/learnflowapi/auth/controller/dto/LoginResponse.java
+++ src/main/java/com/teamexp/learnflowapi/auth/controller/dto/LoginResponse.java
@@ -1,10 +1,29 @@
 package com.teamexp.learnflowapi.auth.controller.dto;
 
+import com.teamexp.learnflowapi.global.security.principal.CustomUserPrincipal;
+import java.time.Instant;
+
 public record LoginResponse(
-    String nickname,
-    String email,
-    String role,
-    String accessToken,
-    String refreshToken
+        String userId,
+        String nickname,
+        String email,
+        String role,
+        String accessToken,
+        String refreshToken,
+        boolean isMembershipActive,
+        Instant membershipExpiryDate
 ) {
+
+    public static LoginResponse create(CustomUserPrincipal user, String accessToken, String refreshToken, MembershipStatus status) {
+        return new LoginResponse(
+                user.getId(),
+                user.getNickname(),
+                user.getEmail(),
+                user.getRole().name(),
+                accessToken,
+                refreshToken,
+                status.isMembershipActive(),
+                status.membershipExpiryDate()
+        );
+    }
 }
