472bcf744b5ec49510afd098a82d85edddbd50e1
dkstpwns10 <75038434+dkstpwns10@users.noreply.github.com>
2026-02-10 14:19:18 +0900

    [Feat] PaymentHistory 도메인 및 서비스 코드 구현 (#118)
    * feat[payment]: TossPayment 구현
    
    * feat[payment]: 토스페이먼츠 연결 및 조회 API 구현
    
    * feat[payment]: 예외처리 세분화 및 응답 값 변경
    
    * fix[payment]: 오타 수정
    
    * fix[payment]: 오타 및 로직 수정
    
    * fix[payment]: LocalDateTime -> Instant변경

---
+++ src/main/java/com/teamexp/learnflowapi/payment/model/PaymentHistory.java
@@ -0,0 +1,72 @@
+package com.teamexp.learnflowapi.payment.model;
+
+import com.teamexp.learnflowapi.payment.model.constant.PaymentStatus;
+import com.teamexp.learnflowapi.payment.model.constant.PlanType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class PaymentHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    @Column(nullable = false, unique = true)
+    private String orderId;
+    private String paymentKey;
+    private Long amount;
+
+    @Enumerated(EnumType.STRING)
+    private PlanType planType;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+    @CreatedDate
+    private Instant createdAt;
+    private Instant approvedAt;
+
+    @Builder
+    private PaymentHistory(String userId, String orderId, String paymentKey,
+                           Long amount, PlanType planType, PaymentStatus status,
+                           Instant approvedAt) {
+        this.userId = userId;
+        this.orderId = orderId;
+        this.paymentKey = paymentKey;
+        this.amount = amount;
+        this.planType = planType;
+        this.status = status;
+        this.approvedAt = approvedAt;
+    }
+
+    public static PaymentHistory create(String userId, String orderId, String paymentKey,
+                                        Long amount, PlanType planType, Instant approvedAt) {
+        return PaymentHistory.builder()
+                .userId(userId)
+                .orderId(orderId)
+                .paymentKey(paymentKey)
+                .amount(amount)
+                .planType(planType)
+                .status(PaymentStatus.DONE)
+                .approvedAt(approvedAt)
+                .build();
+    }
+}
