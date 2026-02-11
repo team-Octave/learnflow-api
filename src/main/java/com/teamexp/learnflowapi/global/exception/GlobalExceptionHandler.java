c1df6b074732a8bf20479f69c9eba5b4332610a3
dkstpwns10 <dkstpwns10@naver.com>
2026-02-11 12:15:36 +0900

    hotfix[develop]: 500에러 원상 복구

--- src/main/java/com/teamexp/learnflowapi/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/teamexp/learnflowapi/global/exception/GlobalExceptionHandler.java
@@ -38,7 +38,7 @@ public class GlobalExceptionHandler {
 
     // 2. 예상하지 못한 에러 처리 (가장 중요 ⭐)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<BaseResponse<String>> handleException(Exception e) {
+    public ResponseEntity<BaseResponse<?>> handleException(Exception e) {
         String traceId = MDC.get("traceId");
 
         // e.printStackTrace() 대신 log.error 사용 (Loki로 스택 트레이스 전체 전송)
